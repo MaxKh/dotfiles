1f23ce3cdaf05caaf1ed99f1d4b1159ba680e4af
Maxim Khaberev <khaberev@gmail.com>
2019-04-18 12:30:44 +0300

    i3pystatus switchable formats

--- .config/i3/i3pystatus_conf.py
+++ .config/i3/i3pystatus_conf.py
@@ -1,12 +1,14 @@
 from i3pystatus import Status
 from i3pystatus.cpu_usage import CpuUsage
 from i3pystatus.disk import Disk
+from i3pystatus.mem import Mem
+from i3pystatus.swap import Swap
 from i3pystatus import Module
 from i3pystatus.core.util import round_dict
 from threading import Thread
 import subprocess
-import sys
 import os
+import sys
 
 class MyCpuUsage(CpuUsage):
     color = "#FFFFFF"
@@ -48,8 +50,45 @@ class MyCpuUsage(CpuUsage):
             return diff_busy / diff_total * 100
 
 
+class FormatSwitcher(object):
+    formats = [""]
+    indx = 0
 
-class MyDiskUsage(Disk):
+    def next_format(self):
+        self.indx = (self.indx + 1) % len(self.formats)
+        self.format = self.formats[self.indx]
+        try:
+            self.send_output()
+        except Exception as ex:
+            pass
+
+
+class MyMemUsage(Mem, FormatSwitcher):
+    settings = (
+        ('formats', '')
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.format=self.formats[0]
+        self.on_rightclick="next_format"
+
+
+class MyDiskUsage(Disk, FormatSwitcher):
+    warn_color = "#FFFF00"
+    alert_color = "#FF0000"
+    percentage_alert_limit = 30
+    percentage_critical_limit = 10
+
+    settings = (
+        ('formats', '')
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.format=self.formats[0]
+        self.on_rightclick="next_format"
+
     def run(self):
         if os.path.isdir(self.path) and not os.path.ismount(self.path):
             if len(os.listdir(self.path)) == 0:
@@ -63,12 +102,21 @@ class MyDiskUsage(Disk):
             return
 
         available = (stat.f_bsize * stat.f_bavail) / self.divisor
+        percentage_avail = stat.f_bavail / stat.f_blocks * 100
 
         if available > self.display_limit:
             self.output = {}
             return
 
-        critical = available < self.critical_limit
+        if self.critical_limit:
+            color = self.alert_color if available < self.critical_limit else self.color
+        else:
+            if percentage_avail < self.percentage_critical_limit:
+                color = self.alert_color
+            elif percentage_avail < self.percentage_alert_limit:
+                color = self.warn_color
+            else:
+                color = self.color
 
         cdict = {
             "total": (stat.f_bsize * stat.f_blocks) / self.divisor,
@@ -76,7 +124,7 @@ class MyDiskUsage(Disk):
             "avail": available,
             "used": (stat.f_bsize * (stat.f_blocks - stat.f_bfree)) / self.divisor,
             "percentage_free": stat.f_bfree / stat.f_blocks * 100,
-            "percentage_avail": stat.f_bavail / stat.f_blocks * 100,
+            "percentage_avail": percentage_avail,
             "percentage_used": (stat.f_blocks - stat.f_bfree) / stat.f_blocks * 100,
         }
         round_dict(cdict, self.round_size)
@@ -84,8 +132,19 @@ class MyDiskUsage(Disk):
         self.data = cdict
         self.output = {
             "full_text": self.format.format(**cdict),
-            "color": self.critical_color if critical else self.color
+            "color": color
         }
+
+
+class MySwapUsage(Swap, FormatSwitcher):
+    settings = (
+        ('formats', '')
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.format=self.formats[0]
+        self.on_rightclick="next_format"
 
 
 class PersistOutputModule(Module):
@@ -134,12 +193,16 @@ class PersistOutputModule(Module):
 
 HINTS = {"separator_block_width": 17}
 HINTS_NO_SEP = dict(HINTS, separator=False)
-# HINTS = {}
 
 status = Status(interval=1)
 
-kbdd = PersistOutputModule(command='~/.config/i3blocks/kbdd', hints=dict(HINTS, min_width='English (US)'), shell=True)
-status.register(kbdd)
+status.register(
+    PersistOutputModule(
+        command='~/.config/i3blocks/kbdd',
+        hints=dict(HINTS, min_width='English (US)'),
+        shell=True
+    )
+)
 
 status.register("clock",
     on_leftclick=["gsimplecal"],
@@ -148,57 +211,80 @@ status.register("clock",
 
 status.register("network",
     interface="eth0",
-    format_up="  {bytes_sent}    {bytes_recv} MB/s",
+    format_up=" {bytes_sent}   {bytes_recv} MB/s",
     divisor=1024 ** 2,
     color_up="#FFFFFF",
     dynamic_color=False,
     hints=HINTS)
 
 
-disk_io = PersistOutputModule(command='~/.config/i3blocks/disk-io -w 3 -M -P 0 -t 1 -s ""', hints=HINTS_NO_SEP, format="  {line}", shell=True)
-status.register(disk_io)
+status.register(
+    PersistOutputModule(
+        command='~/.config/i3blocks/disk-io -w 2 -M -P 0 -t 1 -s " "',
+        hints=HINTS_NO_SEP,
+        format=" {line}",
+        shell=True
+    )
+)
 
-my_cpu_usage = MyCpuUsage(format="  {usage:.2f}%",
-    hints=dict(HINTS_NO_SEP, min_width=" 100.00%"), on_leftclick=['terminator -e "htop"'], interval=2)
-status.register(my_cpu_usage)
+status.register(
+    MyCpuUsage(
+        format=" {usage:.2f}%",
+        hints=dict(HINTS_NO_SEP, min_width=" 00.00%"),
+        on_leftclick=['terminator -e "htop"'],
+        interval=2
+    )
+)
 
-status.register(MyDiskUsage(
-    path="/home",
-    on_leftclick=["pcmanfm-qt ~"],
-    critical_limit=2,
-    format=" HOME {avail} GB",
-    hints=HINTS,
-    interval=10))
+status.register(
+    MyDiskUsage(
+        path="/home",
+        on_leftclick=["pcmanfm-qt ~"],
+        hints=HINTS,
+        interval=10,
+        formats=[" HOME {avail} GB", " HOME {avail} GB free of {total} GB"]
+    )
+)
 
-status.register(MyDiskUsage(
-    path="/workdir",
-    on_leftclick=["pcmanfm-qt /workdir"],
-    critical_limit=2,
-    format=" WORKDIR {avail} GB",
-    hints=HINTS_NO_SEP,
-    interval=10))
+status.register(
+    MyDiskUsage(
+        path="/workdir",
+        on_leftclick=["pcmanfm-qt /workdir"],
+        hints=HINTS_NO_SEP,
+        interval=10,
+        formats=[" WORKDIR {avail} GB", " WORKDIR {avail} GB free of {total} GB"]
+    )
+)
 
-status.register(MyDiskUsage(
-    path="/",
-    on_leftclick=["pcmanfm-qt /"],
-    critical_limit=1,
-    format=" ROOT {avail} GB",
-    hints=HINTS_NO_SEP,
-    interval=10))
+status.register(
+    MyDiskUsage(
+        path="/",
+        on_leftclick=["pcmanfm-qt /"],
+        hints=HINTS_NO_SEP,
+        interval=10,
+        formats=[" ROOT {avail} GB", " ROOT {avail} GB free of {total} GB"]
+    )
+)
 
-status.register("swap",
-    format=" SWAP {free} GB",
-    divisor=1024 ** 3,
-    color='#FFFFFF',
-    hints=HINTS,
-    interval=10)
+status.register(
+    MySwapUsage(
+        formats=[" SWAP {free} GB", " SWAP {free} GB free of {total} GB"],
+        divisor=1024 ** 3,
+        color='#FFFFFF',
+        hints=HINTS,
+        interval=10
+    )
+)
 
-status.register("mem",
-    format=" MEM {avail_mem} GB",
-    divisor=1024 ** 3,
-    color='#FFFFFF',
-    hints=HINTS_NO_SEP,
-    interval=10)
+status.register(
+    MyMemUsage(
+        color='#FFFFFF',
+        hints=HINTS_NO_SEP,
+        interval=10,
+        formats=[" MEM {avail_mem} GB", " MEM {avail_mem} GB free of {total_mem} GB"],
+        divisor=1024**3
+    )
+)
 
 status.register("pulseaudio",
     format="♪ {volume}",
